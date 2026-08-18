ac0f1b986f209f350f66a09ddfaed54527347d13
0x2f0 <git@xn--81b3bxa1f.com>
2026-08-18 13:34:58 +0545

    chore: fetching data from the sheet.

---
+++ app-scripts/sheets.js
@@ -0,0 +1,60 @@
+function updateDataset() {
+	const sheet = SpreadsheetApp.getActiveSpreadsheet();
+	const fileId = sheet.getId();
+
+	let userEmail = Session.getActiveUser().getEmail().toLowerCase();
+	if (!userEmail) {
+		SpreadsheetApp.getUi().alert(
+			"Access Denied",
+			"You are not authenticated to update the dataset!",
+			SpreadsheetApp.getUi().ButtonSet.OK,
+		);
+		return;
+	}
+
+	try {
+		var dfile = DriveApp.getFileById(fileId);
+	} catch (e) {
+		SpreadsheetApp.getUi().alert(
+			"Access Denied",
+			"You are not authorized to update the dataset!",
+			SpreadsheetApp.getUi().ButtonSet.OK,
+		);
+	}
+
+	const isEditor = dfile.getEditors().some((e) => e.getEmail() === userEmail);
+	const isOwner = dfile.getOwner().getEmail() === userEmail;
+
+	const hasEditPermission = isEditor || isOwner;
+	if (!hasEditPermission) {
+		SpreadsheetApp.getUi().alert(
+			"Access Denied",
+			"You are not authorized to update the dataset!",
+			SpreadsheetApp.getUi().ButtonSet.OK,
+		);
+		return;
+	}
+
+	Logger.log("Access Granted, Proceeding with processing data!!");
+
+	// TODO: Get all the file in valid parsable json format.
+	const table = sheet.getSheetValues(1, 1, sheet.getLastRow(), sheet.getLastColumn());
+
+	const jsonData = [];
+	const heading = table[0].map(h => h.toLowerCase());
+	table.forEach((row, index) => {
+		if (index == 0) return;
+		const entry = {};
+		row.forEach((column, index) => {
+			if (!heading[index]) return;
+			entry[heading[index]] = column;
+		});
+
+		jsonData.push(entry);
+	});
+
+	Logger.log("Parsed all the data");
+	Logger.log(JSON.stringify(jsonData, null, 2));
+
+	// TODO: Trigger github action with that data.
+}
