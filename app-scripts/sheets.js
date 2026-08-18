713d741f41dfd4e2e7bd47eaf8e9d6baa92d2c9c
0x2f0 <git@xn--81b3bxa1f.com>
2026-08-18 14:34:57 +0545

    wip: app script to github workflow

--- app-scripts/sheets.js
+++ app-scripts/sheets.js
@@ -40,7 +40,7 @@ function updateDataset() {
 	const table = sheet.getSheetValues(1, 1, sheet.getLastRow(), sheet.getLastColumn());
 
 	const jsonData = [];
-	const heading = table[0].map(h => h.toLowerCase());
+	const heading = table[0].map((h) => h.toLowerCase());
 	table.forEach((row, index) => {
 		if (index == 0) return;
 		const entry = {};
@@ -53,7 +53,39 @@ function updateDataset() {
 	});
 
 	Logger.log("Parsed all the data");
-	Logger.log(JSON.stringify(jsonData, null, 2));
+	// Logger.log(JSON.stringify(jsonData, null, 2));
 
-	// TODO: Trigger github action with that data.
+	const properties = PropertiesService.getScriptProperties();
+
+	const GITHUB_REPO = properties.getProperty("GITHUB_REPO");
+	const WORKFLOW_ID = properties.getProperty("WORKFLOW_ID");
+	const GITHUB_PAT_TOKEN = properties.getProperty("GITHUB_PAT_TOKEN");
+
+	const dataLen = JSON.stringify(jsonData).length;
+	Logger.log(dataLen);
+
+	Logger.log(GITHUB_REPO);
+	Logger.log(GITHUB_PAT_TOKEN);
+	Logger.log(WORKFLOW_ID);
+
+	const response = UrlFetchApp.fetch(
+		`https://api.github.com/repos/${GITHUB_REPO}/actions/workflows/${WORKFLOW_ID}/dispatches`,
+		{
+			method: "post",
+			contentType: "application/json",
+			headers: {
+				"X-GitHub-Api-Version": "2026-03-10",
+				Accept: "application/vnd.github+json",
+				Authorization: `Bearer ${GITHUB_PAT_TOKEN}`,
+				"Content-Type": "application/json",
+			},
+			payload: JSON.stringify({
+				ref: "main",
+				inputs: {
+					jsonData: JSON.stringify(jsonData),
+				},
+			}),
+		},
+	);
+	Logger.log(JSON.stringify(response, null, 2));
 }
