ad8eaec69abca229a63b4bc83e9eccfa8a286e7b
0x2f0 <git@xn--81b3bxa1f.com>
2026-08-23 13:24:26 +0545

    feat: embedding geneation test using `intfloat/multilingual-35-small`
    and fixed the json escapping issue.

--- embedder/index.ts
+++ embedder/index.ts
@@ -1,4 +1,35 @@
-const input = await Bun.stdin.text();
+import { pipeline } from "@huggingface/transformers";
+//
+// const args = Bun.argv.slice(2);
+//
+// if (args.length === 0) {
+// 	console.error("No args provided `jsonData` required.");
+// 	process.exit(1);
+// }
+//
+// const mayBeJsonData = args[0];
+// if (!mayBeJsonData) {
+// 	console.error("undefined arg provided");
+// 	process.exit(1);
+// }
+//
+// try {
+// 	const jsonData = JSON.parse(mayBeJsonData);
+// 	console.log(jsonData);
+// } catch (e) {
+// 	console.error("Invalid json data provided");
+// 	process.exit(1);
+// }
 
-console.log("This is the json data that I will be parsing next");
-console.log(JSON.parse(input));
+const extractor = await pipeline("feature-extraction", "intfloat/multilingual-e5-small");
+const test = [
+	"This framework generates embeddings for each input sentence",
+	"Each sentence is converted into a vector",
+];
+
+const output = await extractor(test, {
+	normalize: true,
+	pooling: "mean",
+});
+
+console.log(output.tolist());
