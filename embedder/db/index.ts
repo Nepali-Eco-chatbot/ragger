b3fcf9ebd916a40bc393b013d6c5874645cedb73
0x2f0 <git@xn--81b3bxa1f.com>
2026-09-11 10:05:24 +0545

    chore: migrated the test to local db

--- embedder/db/index.ts
+++ embedder/db/index.ts
@@ -1,7 +1,12 @@
 import { drizzle } from "drizzle-orm/libsql/node";
 
+const env = process.env.NODE_ENV
+const isTestEnv = env == "test";
+
 export const db = drizzle({
-	connection: {
+	connection: isTestEnv ? {
+		url: "file:local_test.db"
+	} : {
 		url: process.env.DATABASE_URL!,
 		authToken: process.env.DATABASE_AUTH_TOKEN,
 	},
