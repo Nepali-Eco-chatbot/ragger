930380cbfaf5074117e53c9d77e88476a6f93e4b
0x2f0 <git@xn--81b3bxa1f.com>
2026-09-11 09:37:41 +0545

    fix: updated when the data to  table gets updated

--- embedder/helpers/data-processor.ts
+++ embedder/helpers/data-processor.ts
@@ -27,59 +27,65 @@ const BATCH_SIZE = 50;
  * - generates embedding and inserts into the database.
  */
 export const dataProcesser = async (data: TJSONData) => {
-  const stringData = JSON.stringify(data);
-  const entryHash = getHash(stringData);
-  if (await checkHashStore(stringData)) {
-    console.log("Source already present skipping!");
-    return;
-  }
+	const stringData = JSON.stringify(data);
 
-  const fileName = `${entryHash}.${data.type}`;
-  if (DEBUG) console.log("hash calculated");
-  if (DEBUG) console.log("downloading file", fileName);
-  const file = await downloadFile(data, fileName);
-  if (!file && data.type !== "SEARCH_SOURCE") {
-    console.error("[Error]: Something went wrong while downloading the file.");
-    return;
-  }
-  if (DEBUG) console.log("✅ file downloaded");
+	const entryHash = getHash(stringData);
+	if (DEBUG) console.log("hash calculated");
 
-  if (data.type === "SEARCH_SOURCE") {
-    return;
-  }
-  if (DEBUG) console.log("Chunking data");
-  const chunkedData = chunkData({ fileName, data });
-  let embeddedChunks: Promise<TEmbeddedChunk>[] = [];
-  if (DEBUG) console.log("✅ data chunked");
-  if (DEBUG) console.log("updating database");
-  await db.insert(knw_sources).values({
-    id: entryHash,
-    ...data,
-  });
-  if (DEBUG) console.log("✅ database updated");
+	if (await checkHashStore(stringData)) {
+		console.log("Source already present skipping!");
+		return;
+	}
 
-  for await (const chunk of chunkedData) {
-    if (embeddedChunks.length < BATCH_SIZE) {
-      if (DEBUG) console.log("embedding chunk", chunk.contextualized);
-      embeddedChunks.push(embedder.embed(chunk.contextualized));
-      continue;
-    }
+	const fileName = `${entryHash}.${data.type}`;
+	if (DEBUG) console.log("downloading file", fileName);
+	const file = await downloadFile(data, fileName);
+	if (!file && data.type !== "SEARCH_SOURCE") {
+		console.error("[Error]: Something went wrong while downloading the file.");
+		return;
+	}
+	if (DEBUG) console.log("✅ file downloaded");
 
-    const results = await Promise.all(embeddedChunks);
-    if (DEBUG) console.log("Generating embeddings");
-    const values = results.map((result) => {
-      return {
-        source: entryHash,
-        content: result.chunk,
-        embedding: Array.from(result.embedding),
-      };
-    });
+	if (data.type === "SEARCH_SOURCE") {
+		return;
+	}
 
-    if (DEBUG) console.log("Updating embedding to db");
+	if (DEBUG) console.log("Chunking data");
+	const chunkedData = chunkData({ fileName, data });
+	if (DEBUG) console.log("✅ data chunked");
 
-    // we don't need to batch because we are performing only insert operation.
-    await db.insert(pknw_base).values(values);
-    embeddedChunks = [];
-  }
-  if (DEBUG) console.log("✅ embedding generated and inserted to db");
+	let embeddedChunks: Promise<TEmbeddedChunk>[] = [];
+	if (DEBUG) console.log("✅ database updated");
+
+	for await (const chunk of chunkedData) {
+		if (embeddedChunks.length < BATCH_SIZE) {
+			if (DEBUG) console.log("embedding chunk", chunk.contextualized);
+			embeddedChunks.push(embedder.embed(chunk.contextualized));
+			continue;
+		}
+
+		const results = await Promise.all(embeddedChunks);
+		if (DEBUG) console.log("Generating embeddings");
+		const values = results.map((result) => {
+			return {
+				source: entryHash,
+				content: result.chunk,
+				embedding: Array.from(result.embedding),
+			};
+		});
+
+		if (DEBUG) console.log("updating Source database");
+		await db.insert(knw_sources).values({
+			id: entryHash,
+			...data,
+		});
+
+		if (DEBUG) console.log("Updating embedding to db");
+		// we don't need to batch because we are performing only insert operation.
+		await db.insert(pknw_base).values(values);
+
+
+		embeddedChunks = [];
+	}
+	if (DEBUG) console.log("✅ embedding generated and inserted to db");
 };
